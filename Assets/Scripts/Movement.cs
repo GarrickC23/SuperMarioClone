7d270cc7c65d252780c6980d7ea16dc425308e76
GarrickC23 <garrickchiu0723@gmail.com>
2022-08-09 16:33:33 -0700

    Imported Assets

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,6 +7,7 @@ public class Movement : MonoBehaviour
     public Rigidbody2D rb;
     public float speed = 0; 
     private bool isJumping = false;
+    private bool isRunning = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,7 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Jump Button
         if ( Input.GetButtonDown("Jump") && !isJumping )
         {
             Debug.Log("Capsule Jump");
@@ -23,8 +25,27 @@ public class Movement : MonoBehaviour
             rb.velocity = new Vector2(0, 10);
             isJumping = true;
         }
+
+        //Left, Right
         float x = Input.GetAxis("Horizontal");
         transform.position += new Vector3(x, 0, 0) * speed * Time.deltaTime; 
+
+        //Running
+        if ( !isRunning )
+        {
+            speed *= 1; 
+        }
+        if ( Input.GetKeyDown(KeyCode.LeftShift) && !isRunning )
+        {
+            speed *= 5;
+            isRunning = true;
+        }
+
+        //Crouch
+        if ( Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S) )
+        {
+            Debug.Log("crouched");
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
